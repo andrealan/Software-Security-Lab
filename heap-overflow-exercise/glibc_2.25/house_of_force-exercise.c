0200e7571115780a347c92c9d28bbccb98dc0985
andrealan <andrea.lanzi@gmail.com>
2019-12-11 05:10:06 -0500

    add new exercise

--- heap-overflow-exercise/glibc_2.25/house_of_force-exercise.c
+++ heap-overflow-exercise/glibc_2.25/house_of_force-exercise.c
@@ -6,13 +6,15 @@
 
 void win()
 {
-  printf("Congratulations, you win!!! You successfully changed the code flow\n");
+  printf("Congratulations, you win!!! You successfully changed the code flow\n\n");
 }
 
 int main(int argc, char *argv[])
 {
+   unsigned long return_address = 0xbffff224;
    char *buf1, *buf2, *buf3 ;
    int *c ;
+   unsigned long evilsize ;
    int real_size ;
 
    if(argc != 4) return 0;
@@ -22,14 +24,34 @@ int main(int argc, char *argv[])
    buf1 = malloc(256) ;
    strcpy(buf1, argv[1]) ;
 
+   printf("%.8x[%.8x]\n", &return_address, *(&return_address)) ;
+   printf("%.8x[%.8x]\n", &return_address+4, *(&return_address+4)) ;
+   printf("%.8x[%.8x]\n", &return_address+8, *(&return_address+8)) ;
+   printf("%.8x[%.8x]\n", &return_address+12, *(&return_address+12)) ;
+   printf("%.8x[%.8x]\n", &return_address+16, *(&return_address+16)) ;
+
    real_size = malloc_usable_size(buf1) ; 
+
+   printf("The chunk of 256 bytes has been allocated at %p.\n", buf1 - 2);
+   printf("Real size (aligned and all that jazz) of our allocated chunk is %ld.\n", real_size + sizeof(long)*2);	
+
    intptr_t *ptr_top = (intptr_t *) ((char *)buf1 + real_size - sizeof(long)) ;
    printf("Top chunck is: %p\n", ptr_top) ;
-   
-   
+   printf("New size of top chunk %#.8x\n", *((unsigned long long int *)((char *)ptr_top + sizeof(long))));
+
+   evilsize = (unsigned long)return_address - sizeof(long)*4 - (unsigned long)ptr_top;
+   printf("0x%.8x\n", evilsize) ;
    buf2 = malloc(strtoul(argv[2], NULL, 16)) ;
+   printf("As expected, the new pointer is at the same place as the old top chunk: %p\n", buf2 - sizeof(long)*2);
+ 
    buf3 = malloc(256) ;
+   printf("Pointer to the return address value: %p\n", buf3);
    strcpy(buf3, argv[3]) ;
+
+   printf("return:%.8x:[%.8x]\n", return_address, *((int *)(return_address))) ;
+   printf("buf3:%.8x:[%.8x]\n", buf3, *((int *)(buf3))) ;
+   // address of win
+
 
    //free(buf3) ;
    //free(buf2) ;
---
+++ rop/rop-vuln.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char string[100];
+
+void lazy() {
+  system(string);
+}
+
+void food(int magic) {
+  if (magic == 0xdeadbeef) {
+    strcat(string, "/bin");
+  }
+}
+
+void feeling_sick(int magic1, int magic2, filename) {
+  if (magic1 == 0xd15ea5e && magic2 == 0x0badf00d) {
+    strcat(string, "/cat ");
+    strcat(string, "filename") ;
+  }
+}
+
+void vuln(char *string) {
+  char buffer[100] = {0};
+  strcpy(buffer, string); 
+}
+
+int main(int argc, char** argv) {
+  string[0] = 0;
+  char *filename[]="filename";
+
+  printf("me Can I read the secret file?! \n\n");
+  if (argc > 2) {
+    vuln(argv[1]);
+    strcpy(buf, argv[2]) ;
+  } else {
+    printf("You forgot to read me!!!\n");
+  }
+  return 0;
+}
